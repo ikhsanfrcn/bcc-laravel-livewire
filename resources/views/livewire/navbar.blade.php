29efe1615c575cfbc100384842931efd8ea7e1ea
ikhsanfrcn <ikhsanfrcn@gmail.com>
2023-01-23 14:31:36 +0800

    push aja

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -1,26 +1,16 @@
 <nav id="navbar"
     class="z-30 fixed flex flex-wrap items-center justify-between w-[90%] left-[5%] py-4 px-4 text-lg text-gray-700 bg-transparent ease-in-out duration-500 rounded-md">
     <div class="flex items-center flex-shrink-0 text-red-500 mr-6">
-        <a href="#">
-            <svg class="fill-current h-8 w-8 float-left" width="54" height="54" viewBox="0 0 54 54"
-                xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-            </svg>
-            <span class="pl-3 font-semibold text-xl tracking-tight">Tailwind CSS</span>
+        <a href="/">
+            <img src="{{asset ('img/bcc-horizontal-whitefont-1.png')}}" class="h-20" alt="">
         </a>
     </div>
 </nav>
 
 <div class="z-40 fixed hidden w-full items-center ease-in-out pl-[6%] py-32 duration-1000 bg-white" id="menu">
     <div class="flex items-center flex-shrink-0 text-red-500 mr-6">
-        <a href="#">
-            <svg class="fill-current h-8 w-8 float-left" width="54" height="54" viewBox="0 0 54 54"
-                xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-            </svg>
-            <span class="pl-3 font-semibold text-xl tracking-tight">Tailwind CSS</span>
+    <a href="/">
+            <img src="{{asset ('img/bcc-horizontal-whitefont-1.png')}}" class="h-20" alt="">
         </a>
         <ul class=" pt-4 text-base text-black flex justify-between">
             <li>
@@ -63,7 +53,7 @@ window.onscroll = function() {
         document.getElementById("navbar").style.top = "30px";
         // document.getElementById("navbar").style.backgroundColor =  "rgba(55, 65, 81, 0.8)";
     } else {
-        document.getElementById("navbar").style.top = "-70px";
+        document.getElementById("navbar").style.top = "-100px";
         // document.getElementById("navbar").style.backgroundColor =  "rgba(55, 65, 81, 0.8)";
     }
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,12 +2,17 @@
 @section('content')
 
 <header class="relative flex items-center justify-center h-screen overflow-hidden">
-    <div class="relative z-30 grid grid-cols-6 text-white text-center">
-        <div class="col-start-2 col-span-4">
-            <img src="{{ asset('img/we-know-coffee-white-1.png')}}" class="h-12 mx-auto" alt="We Know Coffee" />
-            <h1 class="text-7xl font-extrabold">Start your day off right with Bali Coffee Club</h1>
-            <p class="text-sm px-10">World class coffee, grown and roasted in Bali, it is the perfect way to start any day.
+    <div class="relative z-30 grid md:grid-cols-6 text-white text-center">
+        <div class="pt-20 md:pt-40 px-2 md:px-0 col-start-2 col-span-4">
+            <img src="{{ asset('img/we-know-coffee-white-1.png')}}" class="h-5 md:h-10 mx-auto" alt="We Know Coffee" />
+            <h1 class="pt-5 md:pt-10 text-2xl md:text-5xl lg:text-7xl font-extrabold">Start your day off right with Bali
+                Coffee Club</h1>
+            <p class="pt-5 md:pt-10 text-sm md:px-32">World class coffee, grown and roasted in Bali, it is the perfect
+                way to start
+                any day.
                 Bring the energy of Bali into your morning with every cup of Bali Coffee Clubs.</p>
+            <img src="{{ asset('img/output-onlinepngtools.png')}}" class="pt-10 h-24 md:h-32 mx-auto"
+                alt="We Know Coffee" />
         </div>
     </div>
     <video autoplay loop muted class="absolute z-10 w-auto min-w-full min-h-full max-w-none">
@@ -18,15 +23,59 @@
     </video>
 </header>
 
-<main class="z-40 bg-white">
-    <section class="py-52 grid grid-cols-5">
-        <div class="col-start-2 col-span-3">
-            <h5 class="text-[2vw]">Our Solution</h5>
-            <h1 class="text-[11vw]">Go Forward</h1>
+<main class="z-40 bg-black">
+
+    <section class="py-20 px-5"
+        style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.3), rgba(0,0,0,0.1)), url('https://balicoffeeclub.com/wp-content/uploads/2022/05/Uluwatu-scaled.jpg');">
+        <div class="grid md:grid-cols-2 container mx-auto place-items-center">
+            <div class="text-white">
+                <h5 class="text-[4vw] md:text-[1vw] ">ABOUT BCC COFFEE</h5>
+                <h2 class="pt-10 font-bold text-[6vw] md:text-[2vw]">The Story Behind Bali, the Coffee, and the Club
+                </h2>
+                <p class="py-5 text-justify md:text-left">Bali is home to a rich culture and holds a myriad of ways to
+                    celebrate it. At Bali
+                    Coffee Club, we
+                    celebrate the culture surrounding coffee, which permeates through the many islands of Indonesia.
+                    From
+                    the beautiful land of Bali to your kitchen, wherever it may be, we share a part of our culture
+                    direct to
+                    you.</p>
+                <a href="#">
+                    <button class="bg-white p-3 rounded-md text-[#ff5700]">Know More</button>
+                </a>
+            </div>
+            <div class="md:grid grid-cols-4">
+                <div class="col-start-2 col-span-3">
+                    <img src="{{ asset('img/image-23-1.png')}}"
+                        class="pt-10 md:pt-0  object-cover mx-auto md:rounded-[32px]" alt="Murals" />
+                </div>
+            </div>
+        </div>
+        <div class="grid grid-cols-12 text-white">
+            <div class="col-start-2 col-span-10">
+                <h2 class="py-12 font-bold text-[6vw] md:text-[2vw]">Authentical Product</h2>
+                <div class="grid lg:grid-cols-3 gap-y-10 lg:gap-y-0">
+                    <div class="grid grid-cols-2 place-items-center">
+                        <img src="{{ asset('img/authen-icon-1.png')}}" class="object-cover h-24 md:h-28" alt="Authen" />
+                        <p class="">We source coffee from local Indonesian farmers who practice responsible growing
+                            techniques.</p>
+                    </div>
+                    <div class="grid grid-cols-2 place-items-center">
+                        <img src="{{ asset('img/authen-icon-2.png')}}" class="object-cover h-24 md:h-28" alt="Authen" />
+                        <p class="">We roast our coffee in our world-class roastery nestled in the heart of Bali.</p>
+                    </div>
+                    <div class="grid grid-cols-2 place-items-center">
+                        <img src="{{ asset('img/authen-icon-3.png')}}" class="object-cover h-24 md:h-28" alt="Authen" />
+                        <p class="">Our finished product is packaged and shared out to our club members, locally and
+                            around
+                            the globe.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 
-    <section class="grid md:grid-cols-12">
+    <!-- <section class="pt-32 grid md:grid-cols-12">
         <div class="bg-gray-100 md:col-span-5 md:hidden">
             <img src="" class="h-full" alt="test">
             </img>
@@ -45,7 +94,7 @@
             <img src="" class="h-full" alt="test">
             </img>
         </div>
-    </section>
+    </section> -->
 
 </main>
 <div class="grid grid-cols-12" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.3), rgba(0,0,0,0)">
