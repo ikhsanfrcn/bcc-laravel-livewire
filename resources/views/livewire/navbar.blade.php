87f2783b5407094be51f7b4fc3cfddfa07d655c1
ikhsanfrcn <ikhsanfrcn@gmail.com>
2023-01-26 11:12:53 +0800

    push

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -1,33 +1,37 @@
-
-    <div id="navbar"
-        class="z-30 fixed flex flex-wrap items-center justify-between w-[90%] left-[5%] py-4 px-4 text-lg text-gray-700 bg-transparent ease-in-out duration-500 rounded-md">
-        <div class="flex items-center mr-6">
-            <a href="/">
-                <img src="{{asset ('img/bcc-horizontal-blackfont.png')}}" class="h-20" alt="">
-            </a>
-        </div>
+<div id="navbar"
+    class="z-30 fixed flex flex-wrap items-center justify-between w-[90%] left-[5%] py-4 px-4 text-lg text-gray-700 bg-transparent ease-in-out duration-500 rounded-md">
+    <div class="flex items-center mr-6">
+        <a href="/">
+            <img src="{{asset ('img/bcc-horizontal-blackfont.png')}}" class="h-20" alt="">
+        </a>
     </div>
-    <nav class="hidden md:block">
+</div>
+<nav class="hidden md:block">
     <div class="z-40 fixed hidden w-full items-center justify-between pl-[5%] py-4 px-4 ease-in-out duration-1000 bg-white"
         id="menu">
         <a href="/">
             <img src="{{asset ('img/bcc-horizontal-blackfont.png')}}" class="h-20 ml-4" alt="">
         </a>
-        <div class="mt-12 grid grid-cols-8 py-20">
-            <div class="grid grid-rows-4 gap-y-5 text-black col-start-2 col-span-3 text-3xl">
-                <a class="p-4 py-2 block hover:text-red-400 focus-within:text-black" href="/">HOME</a>
-                <a class="p-4 py-2 block hover:text-red-400" href="about-us">ABOUT US</a>
-                <a class="p-4 py-2 block hover:text-red-400" href="coffeepedia">COFFEEPEDIA</a>
-                <a class="p-4 py-2 block hover:text-red-400" href="contact">CONTACT</a>
-                <a class="p-4 py-2 block hover:text-red-400" href="membership">MEMBERSHIP</a>
+        <div class="mt-12 grid grid-cols-8 py-16">
+            <div class="grid gap-y-5 text-black col-start-2 col-span-3 text-3xl">
+                <a class="block hover:text-red-400 focus-within:text-black" href="/">HOME</a>
+                <a class="block hover:text-red-400" href="about-us">ABOUT US</a>
+                <a class="block hover:text-red-400" href="coffeepedia">COFFEEPEDIA</a>
+                <a class="block hover:text-red-400" href="contact">CONTACT</a>
+                <a class="block hover:text-red-400" href="membership">MEMBERSHIP</a>
             </div>
             <div class="col-start-6 col-span-2 grid grid-rows-3 gap-y-5">
-                <div class="">
+                <div class="row-span-2">
                     <h6 class="text-gray-300">Address</h6>
-                    <p class="text-sm">
+                    <h4 class="text-[2vw] font-semibold">Seminyak</h4>
+                    <p class="text-[1vw]">
                         Gg. Meduri Lingkungan Taman Mertanadi No.5, Kerobokan Kelod, Kec. Kuta Utara, Kabupaten Badung,
                         Bali
                         80361
+                    </p>
+                    <h4 class="text-[2vw] font-semibold">Canggu</h4>
+                    <p class="text-[1vw]]">
+                    Jl. Kubu Manyar No.22, Canggu, Kec. Kuta Utara, Kabupaten Badung, Bali 80361
                     </p>
                 </div>
                 <div class="">
@@ -35,8 +39,6 @@
                     <p class="text-sm">
                         +62 XXXX
                     </p>
-                </div>
-                <div class="">
                     <h6 class="text-gray-300">Enquiries</h6>
                     <p class="text-sm">
                         admin@balicoffeeclub.com
@@ -99,7 +101,7 @@
                         admin@balicoffeeclub.com
                 </div>
             </div> -->
-        </div>
+    </div>
     </div>
 
     <svg xmlns="http://www.w3.org/2000/svg" id="menuMobile-button"
